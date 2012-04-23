8c241d578bc0c8cabdd9f5b85c3ccb659116247a
Rob Terhaar <rob@atlanticdynamic.com>
2012-04-23 00:30:32 -0400

    adding path trace to faced_up()

--- imyface/actions.py
+++ imyface/actions.py
@@ -145,12 +145,18 @@ def faced_up(user_id, face):
 
     """
     def data():
-        d = {'face_enabled': user.user_enabled(face),
+        d = {'user_enabled': user.user_enabled(user_id),
+             'face_enabled': user.user_enabled(face),
              'face_InMyFace': is_in_my_face(user_id, face),
              'path_to_user_from_face': None,
              'user_InMyFace': is_outta_my_face(user_id, face),
              'path_to_face_from_user':None,
             }
+
+        if d['face_InMyFace']:
+            d['path_to_user_from_face'] = find_path(user_id, face)
+            d['path_to_face_from_user'] = find_path(face, user_id)
+
         return d
 
     try:
