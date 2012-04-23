54687ab391639f8ea28ebff5d7ee3d793dbeece0
Rob Terhaar <rob@atlanticdynamic.com>
2012-04-23 01:05:16 -0400

    adding some crappy code for path avg calc

--- imyface/extra_credit.py
+++ imyface/extra_credit.py
@@ -10,4 +10,14 @@ _FACE_DATA = FaceData()
 def minimum_faces(user_id, face):
     """ find the shortest path between user_id->face, then return the length
     """
-    return len(actions.path_finder(user_id, face, find_shortest=True))
+    return len(actions.find_path(user_id, face, find_shortest=True))
+
+def _avg_path_length_for_user(user_id):
+    all_users = _FACE_DATA.data.keys()
+    path_lengths = [minimum_faces(user_id, u) for u in all_users]
+    return sum(path_lengths) / len(path_lengths)
+
+def degrees_of_separation(seps=None):
+    all_users = _FACE_DATA.data.keys()
+    seps = [_avg_path_length_for_user(u) for u in all_users]
+    return sum(seps) / len(seps)
