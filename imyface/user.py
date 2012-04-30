106fab9a651c5013db184538bf684bd337cb882a
Rob Terhaar <rob@atlanticdynamic.com>
2012-04-29 23:02:42 -0400

    adding bigO for each function

--- imyface/user.py
+++ imyface/user.py
@@ -18,23 +18,22 @@ def _new_user(last_name,
               save=False):
     """ Insert user into data dict.
         Each member of the dict must be unique, as identified by the key.
+
+        O(1)
     """
     # add new row to user_data db
     d = {'data': (first_name, last_name, password),'enabled': True}
     _USER_DATA.insert(user_id, d)
 
-#    logging.debug("Added new user %s to user_data" % (user_id))
     # create an empty node in the face_data db
     connect.init_user(user_id)
 
-#    logging.debug("Added new user %s to face_data" % (user_id))
-
-#    if save:
-#        user_data.save()
 
 ## public functions 
 def is_existing_user(user_id):
-    """Check to see if a user exists in the data dict
+    """ Check to see if a user exists in the data dict
+
+        O(1)
     """
     return _USER_DATA.data.has_key((user_id,))
 
@@ -43,6 +42,10 @@ def enroll(first_name,
            user_id,
            password):
     """ Allows a user to provide basic information
+
+        O(1) 
+        ( however this may be as slow as O(n) if there are a lot of 
+        hash collisions on dict insert )
     """
     if not is_existing_user(user_id):
         _new_user(last_name, first_name, user_id, password)
@@ -50,15 +53,27 @@ def enroll(first_name,
         raise UserExists("Found existing user %s in db" % (user_id))
 
 def user_enabled(user_id):
+    """ Check if a user is enabled
+        O(1)
+    """
     return _USER_DATA.data.get((user_id,))['enabled']
 
 def disable_user(user_id):
+    """ Disable a user
+        O(1)
+    """
     _USER_DATA.update_subkey(user_id, 'enabled', False)
 
 def enable_user(user_id):
+    """ Enable a user
+        O(1)
+    """ 
     _USER_DATA.update_subkey(user_id, 'enabled', True)
 
 def get_user(user_id):
+    """ Get user data
+        O(1)
+    """
     u = _USER_DATA.data.get((user_id,), None)
     if u is None:
         return
@@ -66,10 +81,14 @@ def get_user(user_id):
     return user_data
 
 def get_user_count():
+    """ dict keeps track of how many elements it contains
+        O(1)
+    """
     return len(_USER_DATA.data)
 
 def enroll_list(users_list):
     """ Enroll all users in a list
+        O(n)
     """
     users_loaded = 0
     for r in users_list:
